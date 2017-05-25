4a91a347bd12dd1cb43c39a1d1b4ce1d98288753
zhourh <zhourh@getui.com>
2017-05-25 18:00:28 +0800

    update gradle version and add anko support

--- app/src/main/kotlin/com/simplemobiletools/calendar/activities/SplashActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/calendar/activities/SplashActivity.kt
@@ -1,27 +1,21 @@
 package com.simplemobiletools.calendar.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.simplemobiletools.calendar.helpers.DAY_CODE
 import com.simplemobiletools.calendar.helpers.EVENT_ID
+import org.jetbrains.anko.startActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (intent.extras?.containsKey(DAY_CODE) == true) {
-            Intent(this, DayActivity::class.java).apply {
-                putExtra(DAY_CODE, intent.getStringExtra(DAY_CODE))
-                startActivity(this)
-            }
+            startActivity<DayActivity>(DAY_CODE to intent.getStringExtra(DAY_CODE))
         } else if (intent.extras?.containsKey(EVENT_ID) == true) {
-            Intent(this, EventActivity::class.java).apply {
-                putExtra(EVENT_ID, intent.getIntExtra(EVENT_ID, 0))
-                startActivity(this)
-            }
+            startActivity<DayActivity>(EVENT_ID to intent.getIntExtra(EVENT_ID, 0))
         } else {
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity<MainActivity>()
         }
         finish()
     }
